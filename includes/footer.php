5e763b6abbbbb8e7affe2ff2383def0b74c1a60b
jamesl1001 <jamesl1001@msn.com>
2014-01-30 14:20:40 +0000

    Update copyright to 2014

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy; Precise Pixels 2010 - 2013. All rights reserved.</p>
+    <p>&copy; Precise Pixels 2010 - 2014. All rights reserved.</p>
 
 	<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FPrecisePixels%3Fref%3Dts%26fref%3Dts&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;font=verdana&amp;colorscheme=light&amp;action=like&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:80px; height:21px;" allowTransparency="true"></iframe>
 
